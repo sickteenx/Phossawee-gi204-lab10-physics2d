21a16129b49022040c3e9a23f0c5386dec64e576
sickteenx <bestzerzr@gmail.com>
2025-04-25 02:56:52 +0700

    vdo2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,18 +3,48 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb2d; //private variable
-
     float move; //store Input from player
     [SerializeField] float speed;
+
+    //Jump
+    [SerializeField] float jumpForce;
+    [SerializeField] bool isJumping;
+
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
 
+
     void Update()
     {
+        //walk left-right
         move = Input.GetAxis("Horizontal");
         rb2d.linearVelocity = new Vector2(move * speed, rb2d.linearVelocity.y);
+
+        //Jump
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb2d.AddForce(new Vector2(rb2d.velocity.x, jumpForce));
+            Debug.Log("Jump!"); //for debugging
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
+        }
+
     }
 }
